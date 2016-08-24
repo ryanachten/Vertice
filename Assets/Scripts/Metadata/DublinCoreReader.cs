bb45d0c1740fe521536a40db8c97dac8ccccf92e
Tim <tim.bathgate@gmail.com>
2016-08-24 18:00:55 +1200

    Add dummy DublinCoreReader
    The DublinCoreReader class implements a skeleton XML reader that
    emits a Dictionary<string, Dictionary<string, string[]>> via its
    GetArtefactWithIdentifier(...) method.
    
    Calling DublinCoreReader.GetArtefactWithIdentifier will return a
    dictionary that is equivalent in structure to the following XML
    representation:
    
    <artefact id="ID1234/19/A">
        <descriptive>
          <title>Another title for another original resource</title>
          <title>Another title for that original resource, maybe in some other language?</title>
          <title>Yet another title, for that original resource</title>
          <creator>Some Guy</creator>
          <creator>Some Guy's Friend</creator>
          <description>Lorem ipsum...</description>
          <format>Statue</format>
          <identifier>ID1234/19/A</identifier>
          <relation>http://themuseum.org/catalogue/ID123419A</relation>
        </descriptive>
        <structural>
          <creator>Digitisation Guy</creator>
          <created>2015-01-25</created>
          <identifier>ID1234.19.A.obj</identifier>
          <identifier>ID1234.19.A.png</identifier>
          <isVersionOf>http://themuseum.org/repository/ID123419A.zip</isVersionOf>
        </structural>
      </artefact>
    
    The first title can be accessed using a combination of dictionary and
    array accessors:
    
    Dictionary<string, Dictionary<string, string[]>> data = DublinCoreReader.GetArtefactWithIdentifier("EXAMPLE");
    for (int i = 0; i < data["descriptive"]["title"].Length; i++){
    	string theTitle = data["descriptive"]["title"][i];
    	print (theTitle);
    
    The dummy method should be used for UI testing until the proper
    functionality can be implemented

---
+++ Assets/Scripts/Metadata/DublinCoreReader.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+/// <summary>
+/// The DublinCoreReader reads in XML data from a specified file. The pattern of 
+/// use for this class is as follows:
+/// 
+/// DublinCoreReader.LoadXml("https://www.example.com/metadata/metadata_file.xml"); // You MUST associate a metadata file before doing ANYTHING else
+/// ...
+/// ...
+/// DublinCoreReader.Refresh() // Call this if you want to check that the metadata file is up to date
+/// </summary>
+public static class DublinCoreReader {
+
+	static XmlDocument _xmlDocument;
+	static string _uri;
+
+	/// <summary>
+	/// Loads XML data from a given filepath or URL
+	/// </summary>
+	/// <param name="filePath">File path.</param>
+	public static void LoadXml(string uri){
+		_uri = uri;
+		Refresh ();
+	}
+
+	/// <summary>
+	/// Synchronise the system's copy of the artefact library with the XML file stored on disk
+	/// </summary>
+	public static void Refresh(){
+
+		if (_uri == null) {
+			Debug.Log ("The URI for XML data is not set. Did you forget to call LoadXml with a URI before calls to RefreshXML?");
+			throw new FileNotFoundException ();
+		}
+
+		// FIXME Loading in XML data will block until the data is returned. Using coroutines isn't a solution, since DublinCoreReader is not
+		// a subclass of MonoBehvaiour
+		WWW www = new WWW (_uri);
+		while (!www.isDone) {
+		}
+		_xmlDocument = new XmlDocument ();
+		_xmlDocument.LoadXml (www.text);
+
+
+	}
+
+	/// <summary>
+	/// Lazily read in XML data to the local _xmlDocument variable
+	/// </summary>
+	/// <returns>The XML document representing artefacts in the system</returns>
+	static XmlDocument Xml(){
+		if (_xmlDocument == null) {
+			Refresh ();
+		}
+		return _xmlDocument;
+	}
+
+	public static Dictionary<string, Dictionary<string, string[]>> GetArtefactWithIdentifier(string identifier){
+		Dictionary<string, Dictionary<string, string[]>> retVal = new Dictionary<string, Dictionary<string, string[]>> ();
+//		XmlNode artefact = Xml().SelectSingleNode(String.Format("//artefact[@id='{0}'", identifier));
+//		XmlNode descriptive = artefact.SelectSingleNode ("/descriptive");
+//		XmlNode structural = artefact.SelectSingleNode ("/structural");
+
+		// RETURN DUMMY DATA
+		retVal.Add("descriptive", new Dictionary<string, string[]>());
+		retVal.Add("structural", new Dictionary<string, string[]>());
+		retVal["descriptive"].Add ("title", new string[] {
+			"Another title for another original resource",
+			"Another title for that original resource, maybe in some other language?",
+			"Yet another title, for that original resource"
+		});
+		retVal["descriptive"].Add ("creator", new string[] {"Some Guy", "Some Guy's Friend"});
+		retVal["descriptive"].Add ("description", new string[] {"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit blandit massa vitae ultrices. Fusce ligula augue, ullamcorper non bibendum in, sollicitudin et risus. Maecenas eget elementum massa, vitae sagittis urna. Mauris vestibulum erat et volutpat fermentum. Nulla rutrum nisi enim, eget rhoncus ante condimentum ac. Phasellus nec eleifend eros. Vestibulum vitae molestie nisl, vitae porttitor mi. Pellentesque convallis justo a suscipit scelerisque. Pellentesque in luctus turpis, eu pretium nunc. Vivamus libero massa, ultrices in tempor ac, varius a diam. Sed nec urna quis nisi viverra volutpat sed quis arcu. Donec pharetra ex vel fringilla viverra. Cras quis congue mi."});
+		retVal["descriptive"].Add ("format", new string[] { "Statue" });
+		retVal["descriptive"].Add ("identifier", new string[] {"ID1234/19/A"});
+		retVal["descriptive"].Add ("relation", new string[] {"http://themuseum.org/catalogue/ID123419A"});
+
+
+		retVal["structural"].Add ("creator", new string[] { "Digitisation Guy" });
+		retVal["structural"].Add ("created", new string[] { "2015-01-25" });
+		retVal["structural"].Add ("identifier", new string[] { "ID1234.19.A.obj", "ID1234.19.A.png" });
+		retVal["structural"].Add ("isVersionOf", new string[] { "http://themuseum.org/repository/ID123419A.zip" });
+
+		return retVal;
+
+
+	}
+}
---
+++ Assets/Scripts/Metadata/Editor/TestDublinCoreReader.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+public class TestDublinCoreReader {
+
+	[Test]
+	public void TestDummyData(){
+		Dictionary<string, Dictionary<string, string[]>> data = DublinCoreReader.GetArtefactWithIdentifier ("THIS WONT DO ANYTHING");
+
+		// Ensure that the nested dictionary structure exists
+		Assert.NotNull (data);
+		Assert.NotNull (data ["descriptive"]);
+		Assert.NotNull (data ["structural"]);
+
+		// Ensure that the structure of the dictionary is as expected
+		Assert.That(data.Keys.Count == 2);
+		Assert.That(data["descriptive"].Keys.Count == 6);
+		Assert.That(data["structural"].Keys.Count == 4);
+
+		// Ensure that the length of the arrays in the 'leaves' is as expected
+		//
+		// <descriptive>
+		Assert.That(data["descriptive"]["title"].Length == 3);
+		Assert.That(data["descriptive"]["creator"].Length == 2);
+		Assert.That(data["descriptive"]["description"].Length == 1);
+		Assert.That(data["descriptive"]["format"].Length == 1);
+		Assert.That(data["descriptive"]["identifier"].Length == 1);
+		Assert.That(data["descriptive"]["relation"].Length == 1);
+
+		// <structural>
+		Assert.That(data["structural"]["creator"].Length == 1);
+		Assert.That(data["structural"]["created"].Length == 1);
+		Assert.That(data["structural"]["identifier"].Length == 2);
+		Assert.That(data["structural"]["isVersionOf"].Length == 1);
+
+		// Ensure that (some of) the actual data is correct
+		Assert.That (data ["descriptive"] ["title"] [0] == "Another title for another original resource");
+		Assert.That (data["descriptive"]["title"][1] == "Another title for that original resource, maybe in some other language?");
+		Assert.That (data ["structural"] ["identifier"] [0] == "ID1234.19.A.obj");
+
+	}
+}
