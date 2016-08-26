00128cdbdc50930e99fbb6a995fea726d97b7203
Ryan Achten <ryanachten@gmail.com>
2016-08-26 19:23:55 +1200

    Begin Browse context panel test. Working communication with DublinCoreReader

---
+++ Assets/GuiReDesContent/Vertice_GuiScripts/Vertice_GuiScripts_Utility/ContextPanel_TestController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ContextPanel_TestController : MonoBehaviour {
+
+	public string testIdentifier;
+//	public Text titleText;
+
+	void Start()
+	{
+		DublinCoreReader.LoadXml("file://" + Application.dataPath + "/Scripts/Metadata/TestAssets/Metapipe_ObjArchive_Subset_As_DublinCore.xml");
+		Dictionary<string, Dictionary<string, string[]>> data = DublinCoreReader.GetArtefactWithIdentifier(testIdentifier);
+
+		for (int i = 0; i < data["descriptive"]["title"].Length; i++){
+			string theTitle = data["descriptive"]["title"][i];
+			Debug.Log(theTitle);
+		}
+	}
+
+
+
+}
