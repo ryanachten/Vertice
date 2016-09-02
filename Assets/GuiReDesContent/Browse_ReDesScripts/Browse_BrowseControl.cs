38b2f542f4539dd33e2e4d40ce9913cd85a6b64b
Ryan Achten <ryanachten@gmail.com>
2016-09-02 20:00:41 +1200

    Define instantiation points for browse functionality and array to hold positions

--- Assets/GuiReDesContent/Browse_ReDesScripts/Browse_BrowseControl.cs
+++ Assets/GuiReDesContent/Browse_ReDesScripts/Browse_BrowseControl.cs
@@ -3,6 +3,29 @@ using System.Collections;
 
 public class Browse_BrowseControl : MonoBehaviour {
 
+	//Controls the instantiation of artefacts to Browse scene after user browse query
+
+	public Transform instantParent;
+	private Transform[] instantPoints;
+
+	void Start()
+	{
+		GetInstantPoints();
+	}
+
+
+	/// <summary>
+	/// Gets transforms from editor mockup to assign to instantiated artefacts
+	/// </summary>
+	void GetInstantPoints ()
+	{
+		instantPoints = new Transform[instantParent.childCount];
+
+		for (int i = 0; i < instantPoints.Length; i++) {
+			instantPoints[i] = instantParent.GetChild(i);
+			Debug.Log("instantPoint " + i + ": " + instantPoints[i].position);
+		}
+	}
 
 	/// <summary>
 	/// Uses DCReader to return a list of artefacts relevant to user search query
@@ -15,5 +38,6 @@ public class Browse_BrowseControl : MonoBehaviour {
 		//alternatively, could just have a function to expose a list of all the artefacts in the XML and I can write logic to test against this
 
 	}
+
 
 }
