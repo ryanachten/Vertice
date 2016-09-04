35e7c8623101830bdf2dd5a666a38afb25cf23aa
Tim <tim.bathgate@gmail.com>
2016-09-04 14:20:39 +1200

    Add unit test stubs

---
+++ Assets/Scripts/Metadata/Editor/TestDublinCoreBrowse.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class TestDublinCoreBrowse {
+
+	[Test]
+	public void TestBrowseByCreator()
+	{
+		Assert.Fail ();
+	}
+
+	[Test]
+	public void TestBrowseByContributor()
+	{
+		Assert.Fail ();
+	}
+
+	[Test]
+	public void TestBrowseByDate()
+	{
+		Assert.Fail ();
+	}
+
+	[Test]
+	public void TestBrowseBySubject()
+	{
+		Assert.Fail ();
+	}
+
+	[Test]
+	public void TestBrowseByCoverage()
+	{
+		Assert.Fail ();
+	}
+}
---
+++ Assets/Scripts/Metadata/Editor/TestDublinCoreSearch.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class TestDublinCoreSearch {
+
+	[Test]
+	public void TestSearchByTermOverFields_01()
+	{
+		// Test searching over a single field
+		Assert.Fail ();
+	}
+
+	[Test]
+	public void TestSearchByTermOverFields_02()
+	{
+		// Test searching over a two fields
+		Assert.Fail ();
+	}
+
+	[Test]
+	public void TestSearchByTermOverFields_03()
+	{
+		// Test an empty search string
+		Assert.Fail ();
+	}
+
+	[Test]
+	public void TestSearchByTermOverFields_04()
+	{
+		// Test an empty set of fields
+		Assert.Fail ();
+	}
+
+	[Test]
+	public void TestSearchByTermOverFields_05()
+	{
+		// Test field does not exist
+		Assert.Fail ();
+	}
+
+	[Test]
+	public void TestSearchByTermOverFields_06()
+	{
+		// Test term does not exist
+		Assert.Fail ();
+	}
+}
