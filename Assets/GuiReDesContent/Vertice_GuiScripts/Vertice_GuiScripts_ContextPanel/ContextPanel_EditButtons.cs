54b0ed674007635983312dc656beaae048f5e400
Ryan Achten <ryanachten@gmail.com>
2016-10-06 15:45:36 +1300

    Finish modify GUI help guides. Begin webGL GUI preprocessor directives.

---
+++ Assets/GuiReDesContent/Vertice_GuiScripts/Vertice_GuiScripts_ContextPanel/ContextPanel_EditButtons.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContextPanel_EditButtons : MonoBehaviour {
+
+	void Start () 
+	{
+		#if UNITY_WEBGL
+		gameObject.SetActive(false);
+		#endif
+	}
+}
---
+++ Assets/GuiReDesContent/Vertice_GuiScripts/Vertice_LoadScene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vertice_LoadScene : MonoBehaviour {
+
+
+
+	public void loadImport()
+	{
+		Application.LoadLevel("MetaPipe_ImportScene");	
+	}
+		
+	public void loadBrowse()
+	{	
+		Application.LoadLevel("MetaPipe_BrowseScene");
+	}
+
+
+	public void loadCollection()
+	{	
+		Application.LoadLevel("MetaPipe_CollectionScene");
+	}
+}
