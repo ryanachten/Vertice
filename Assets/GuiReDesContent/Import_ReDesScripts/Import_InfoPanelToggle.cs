b2da25ee1a520ce6da93d141059d09c0831efc2c
Ryan Achten <ryanachten@gmail.com>
2016-10-22 12:25:00 +1300

    Import scene GUI updates

---
+++ Assets/GuiReDesContent/Import_ReDesScripts/Import_InfoPanelToggle.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Import_InfoPanelToggle : MonoBehaviour {
+
+	public GameObject artefactPanel;
+	public GameObject contextPanel;
+	public GameObject originalPanel;
+	public GameObject mediaPanel;
+	public GameObject meshPanel;
+
+	void Start()
+	{
+		TogglePanels("artefact");
+	}
+
+
+	public void TogglePanels( string infoMode)
+	{
+		switch (infoMode) {
+
+		case "artefact" :
+			artefactPanel.SetActive(true);
+			contextPanel.SetActive(false);
+			originalPanel.SetActive(false);
+			mediaPanel.SetActive(false);
+			meshPanel.SetActive(false);
+			break;
+
+		case "context" :
+			artefactPanel.SetActive(false);
+			contextPanel.SetActive(true);
+			originalPanel.SetActive(false);
+			mediaPanel.SetActive(false);
+			meshPanel.SetActive(false);
+			break;
+
+		case "original" :
+			artefactPanel.SetActive(false);
+			contextPanel.SetActive(false);
+			originalPanel.SetActive(true);
+			mediaPanel.SetActive(false);
+			meshPanel.SetActive(false);
+			break;
+
+		case "media" :
+			artefactPanel.SetActive(false);
+			contextPanel.SetActive(false);
+			originalPanel.SetActive(false);
+			mediaPanel.SetActive(true);
+			meshPanel.SetActive(false);
+			break;
+
+		case "mesh" :
+			artefactPanel.SetActive(false);
+			contextPanel.SetActive(false);
+			originalPanel.SetActive(false);
+			mediaPanel.SetActive(false);
+			meshPanel.SetActive(true);
+			break;
+
+		default:
+			break;
+		}
+	}
+}
