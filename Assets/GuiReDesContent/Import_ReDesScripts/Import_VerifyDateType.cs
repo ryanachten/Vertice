b074975476faf625e694b7a5150893608a2c6301
Ryan Achten <ryanachten@gmail.com>
2016-11-14 15:37:22 +1300

    Add restrictions and feedback surrounding Date and Created input to align with DateTime input expectations.

---
+++ Assets/GuiReDesContent/Import_ReDesScripts/Import_VerifyDateType.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Import_VerifyDateType : MonoBehaviour {
+
+	public InputField dateInputField;
+
+	public void VerifyDateInput()
+	{
+		if (dateInputField.text.Length > 0)
+		{
+			try 
+			{
+				System.DateTime dateType = System.DateTime.Parse(dateInputField.text);
+				dateType.ToString("yyyy/mm/dd");
+			}
+			catch (System.Exception ex) 
+			{
+				StartCoroutine(ErrorFeedback());
+			}
+		}
+	}
+
+	IEnumerator ErrorFeedback()
+	{
+		dateInputField.text = "Date format must be [YYYY/MM/DD]"; 
+
+		yield return new WaitForSeconds(3);
+
+		dateInputField.text = "";
+	}
+}
